80dcf6c1727b4919f6f1b2446f3947c1ed22857f
trunganhvu <vutrunganh1@gmail.com>
2021-09-26 21:48:57 +0700

    update order: html, css, form infor

--- mainapp/view/Order/AddressController.py
+++ mainapp/view/Order/AddressController.py
@@ -6,6 +6,10 @@ from django.conf import settings
 from django.core.cache import cache
 
 LOAD_ADDRESS_FILE = 'load-address-file'
+GET_ALL_CITY = 'get-all-city' 
+GET_ALL_DISTRICTS_IN_CITY = 'get-all-districts-in-city-' 
+GET_ALL_WARDS_IN_CITY = 'get-all-wards-in-city-'
+
 
 def load_address_file():
     """
@@ -27,73 +31,119 @@ def get_all_city(request):
     """
     API get all city
     """
-    # Get all data
-    data = load_address_file()
+    # Check exist in cache
+    cached_data = cache.get(GET_ALL_CITY)
+    if not cached_data:
+        # Get all data
+        data = load_address_file()
 
-    list_city = []
+        list_city = []
 
-    for da in data:
-        city = {
-            'name': da['name'],
-            'code': da['code']
-        }
-        list_city.append(city)
-    return Response(list_city)
+        for da in data:
+            city = {
+                'name': da['name'],
+                'code': da['code']
+            }
+            list_city.append(city)
+
+        # Set into cache
+        cache.set(GET_ALL_CITY, list_city, settings.CACHE_TIME)
+        cached_data = list_city
+    
+    return Response(cached_data)
+
+def get_all_city2():
+    """
+    Getl all city 
+    """
+    # Check exist in cache
+    cached_data = cache.get(GET_ALL_CITY)
+    if not cached_data:
+        # Get all data
+        data = load_address_file()
+
+        list_city = []
+
+        for da in data:
+            city = {
+                'name': da['name'],
+                'code': da['code']
+            }
+            list_city.append(city)
+
+        # Set into cache
+        cache.set(GET_ALL_CITY, list_city, settings.CACHE_TIME)
+        cached_data = list_city
+    return cached_data
 
 @api_view(['GET'])
 def get_all_districts_in_city(request, city_code):
     """
     Get all districts in city
     """
-    # Get all data
-    data = load_address_file()
+    # Check exist in cache
+    key_cache = GET_ALL_DISTRICTS_IN_CITY + str(city_code)
+    cached_data = cache.get(key_cache)
+    if not cached_data:
+        # Get all data
+        data = load_address_file()
+        list_district = []
 
-    list_district = []
-
-    # Loop all data
-    for da in data:
-        # If is city
-        if da['code'] == city_code:
-            districts = da['districts']
-
-            # Get all district 
-            for district in districts:
-                d = {
-                    'name': district['name'],
-                    'code': district['code'],
-                }
-                list_district.append(d)
-            break
-    return Response(list_district)
+        # Loop all data
+        for da in data:
+            # If is city
+            if da['code'] == city_code:
+                districts = da['districts']
+                # # Get all district 
+                for district in districts:
+                    # print(district)
+                    d = {
+                        'name': district['name'],
+                        'code': district['code'],
+                    }
+                    list_district.append(d)
+                # print(list_district)
+                break
+        # Set into cache
+        cache.set(key_cache, list_district, settings.CACHE_TIME)
+        cached_data = list_district
+    return Response(cached_data)
 
 @api_view(['GET'])
 def get_all_wards_in_city(request, city_code, district_code):
     """
     Get all wards in city
     """
-    # Get all data
-    data = load_address_file()
+    # Check exist in cache
+    key_cache = GET_ALL_WARDS_IN_CITY + str(city_code) + '-' + str(district_code)
+    cached_data = cache.get(key_cache)
+    if not cached_data:
+        # Get all data
+        data = load_address_file()
 
-    list_ward = []
+        list_ward = []
 
-    # Loop all data
-    for da in data:
-        # If is city
-        if da['code'] == city_code:
-            districts = da['districts']
+        # Loop all data
+        for da in data:
+            # If is city
+            if da['code'] == city_code:
+                districts = da['districts']
 
-            # Get all district 
-            for district in districts:
+                # Get all district 
+                for district in districts:
 
-                if district['code'] == district_code:
-                    wards = district['wards'] 
-                    for ward in wards:
-                        w = {
-                            'name': ward['name'],
-                            'code': ward['code']
-                        }
-                        list_ward.append(w)
-                break
-        break
-    return Response(list_ward)
+                    if district['code'] == district_code:
+                        wards = district['wards'] 
+                        for ward in wards:
+                            w = {
+                                'name': ward['name'],
+                                'code': ward['code']
+                            }
+                            list_ward.append(w)
+                    break
+            break
+        # Set into cache
+        cache.set(key_cache, list_ward, settings.CACHE_TIME)
+        cached_data = list_ward
+    return Response(cached_data)
 
--- mainapp/view/Order/OrderInforController.py
+++ mainapp/view/Order/OrderInforController.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render, redirect
 
-from mainapp.Common import ConstValiable
 from django.contrib.auth.decorators import login_required
 from mainapp.service.Cart import CartService
 from mainapp.service.Product import ProductDetailService, ProductService, ProductImageService
 from mainapp.service.ProductType import ProductTypeService
+from mainapp.view.Order import AddressController
+import math
 
 def view_info_order(request):
     """
@@ -16,11 +17,14 @@ def view_info_order(request):
             cart_detail_id = request.POST.getlist('cart-detail-id')
 
             list_product_item_cart = []
+            list_city = []
             uc_code = request.COOKIES['uc_code']
 
             # check key code exist
             cart = CartService.get_cart_by_key_code(uc_code)
             if cart is not None:
+                # list city
+                list_city = AddressController.get_all_city2()
                 for detail_id in cart_detail_id:
                     c_detail = CartService.get_cart_detail_by_cart_detail_id(detail_id)
 
@@ -28,10 +32,13 @@ def view_info_order(request):
                         product = c_detail.product_detail_id.product_id
 
                         product_detail = c_detail.product_detail_id
+                        product_detail.product_public_price = math.floor(product_detail.product_public_price)
 
                         product_size = c_detail.product_detail_id.product_size_id
 
                         product_color = c_detail.product_detail_id.product_color_id
+
+                        product_image = ProductImageService.get_one_product_image_by_product_id(product.product_id)
 
                         product_item = {
                             'product': product,
@@ -39,10 +46,13 @@ def view_info_order(request):
                             'product_size': product_size,
                             'product_color': product_color,
                             'quantity': c_detail.quantity,
+                            'product_image': product_image,
+                            'total_price_item': product_detail.product_public_price * c_detail.quantity,
                         }
                         list_product_item_cart.append(product_item)
             context = {
-                'list_product_item_cart': list_product_item_cart
+                'list_product_item_cart': list_product_item_cart,
+                'list_city': list_city,
             }
             return render(request, 'public/Order/orderinfor.html', context=context)
         else:
